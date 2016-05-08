8df7f53f2b8d0febc354d7fb9dbe3d4a05a71404
Muhammad Safwan Abdullah <saf1wan@users.noreply.github.com>
2016-05-08 23:13:03 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -14,27 +14,3 @@ data$Datetime <- as.POSIXct(datetime)
 # Generating Plot 1
 hist(data$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
 png('plot1.png', width = 480, height = 480)
-
-# Generating Plot 2
-plot(data$Global_active_power ~ data$Datetime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
-
-
-# Generating Plot 3
-with(data, {plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
-lines(Sub_metering_2 ~ Datetime, col = 'Red')
-lines(Sub_metering_3 ~ Datetime, col = 'Blue')
-})
-
-legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-## Generating Plot 4
-par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
-with(data, {plot(Global_active_power ~ Datetime, type = "l", ylab = "Global Active Power", xlab = "")
-plot(Voltage ~ Datetime, type = "l", ylab = "Voltage", xlab = "datetime")
-plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Energy sub metering", xlab = "")
-lines(Sub_metering_2 ~ Datetime, col = 'Red')
-lines(Sub_metering_3 ~ Datetime, col = 'Blue')
-legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, bty = "n", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-plot(Global_reactive_power ~ Datetime, type = "l", ylab = "Global_rective_power", xlab = "datetime")
-})
-
