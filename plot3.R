8cf650be2ea553d1d206fcf77036cb0751893d9a
Muhammad Safwan Abdullah <saf1wan@users.noreply.github.com>
2016-05-08 23:17:45 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -6,3 +6,4 @@ lines(Sub_metering_3 ~ Datetime, col = 'Blue')
 
 legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 png('plot3.png', width = 480, height = 480)
+
